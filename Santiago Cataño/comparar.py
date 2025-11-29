0f8462654b05fe87c7dfbc905f066dddb708f451
santi06152002 <78134997+santi06152002@users.noreply.github.com>
2025-11-29 10:33:07 -0500

    Add files via upload

---
+++ Santiago Cataño/comparar.py
@@ -0,0 +1,11 @@
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+
+#Preguntar problema de mayor y menor
+
+if num1 < num2:
+    print(f"El numero {num1} es mayor.")
+elif num1 > num2:
+    print(f"El numero {num2} es mayor.")
+else:
+    print(f"Ambos números {num1} y {num2} son iguales.")
