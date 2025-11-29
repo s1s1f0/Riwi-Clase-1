975633bff7dcf71cdea35242861990321deaee4c
main_santiago <78134997+s1s1f0@users.noreply.github.com>
2025-11-29 13:41:54 -0500

    Add files via upload

---
+++ Clase Johan/functions.py
@@ -0,0 +1,14 @@
+
+def suma(num1,num2):
+
+
+    resultado = num1 + num2
+
+    #print(f"{resultado}")
+
+    return resultado
+
+print(suma(2,3))
+
+#suma(2,3)
+
---
+++ Clase Johan/vocals.py
@@ -0,0 +1,13 @@
+def contar_vocales(num_letras):
+    vocales = num_letras.lower
+
+    for letra in range (6):
+        print(isinstance(letra))
+        if letra >= 5:
+            print(f"El total de vocales es: {letra}")
+        else:
+            print("Error")
+
+   
+   
+
